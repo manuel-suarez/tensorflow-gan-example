b23d7f9f60cd896cec1f3ca0a98e8a2598aae6bc
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-16 14:28:06 -0600

    Configure data batches

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ from IPython import display
 (train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
 train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
 train_images = (train_images - 127.5) / 127.5 # normalize the images to [-1, 1]
+# Constants definition
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+# Batch and shuffle the data
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)

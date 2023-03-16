0bac333aec94efdbebb9ab9c36b6520f9d57ce27
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-16 14:26:25 -0600

    Data loading

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ from tensorflow.keras import layers
 import time
 
 from IPython import display
+
+# Data loading
+(train_images, train_labels), (_, _) = tf.keras.datasets.mnist.load_data()
+train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5 # normalize the images to [-1, 1]
